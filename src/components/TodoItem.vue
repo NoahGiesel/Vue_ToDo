7d01292e45e585702f12b6f03ea390a752f2a454
Noah Giesel <noahgiesel@gmail.com>
2020-02-26 12:41:11 +0100

    todo list in vue finished using axios and placeholderApi

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-completed':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+            {{todo.title}}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {  
+            this.todo.completed = !this.todo.completed
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    .todo-item { 
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-completed { 
+        text-decoration: line-through;
+    }
+
+    .del {
+        background-color: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right
+    }
+</style>
